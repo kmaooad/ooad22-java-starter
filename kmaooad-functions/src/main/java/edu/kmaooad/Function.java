63c613d80bd1ec842b758fc221d30c72a5b12621
Anton <a@bnsk.pro>
2022-10-14 11:04:13 +0300

    Try CI

--- kmaooad-functions/src/main/java/edu/kmaooad/Function.java
+++ kmaooad-functions/src/main/java/edu/kmaooad/Function.java
@@ -8,33 +8,40 @@ import com.microsoft.azure.functions.HttpStatus;
 import com.microsoft.azure.functions.annotation.AuthorizationLevel;
 import com.microsoft.azure.functions.annotation.FunctionName;
 import com.microsoft.azure.functions.annotation.HttpTrigger;
-
 import java.util.Optional;
 
 /**
  * Azure Functions with HTTP Trigger.
  */
 public class Function {
-    /**
-     * This function listens at endpoint "/api/TelegramWebhook". To invoke it using "curl" command in bash:
-     * curl -d "HTTP Body" {your host}/api/TelegramWebhook
-     */
-    @FunctionName("TelegramWebhook")
-    public HttpResponseMessage run(
-            @HttpTrigger(
-                name = "req",
-                methods = {HttpMethod.POST},
-                authLevel = AuthorizationLevel.FUNCTION)
-                HttpRequestMessage<Optional<String>> request,
-            final ExecutionContext context) {
-        context.getLogger().info("Java HTTP trigger processed a request.");
 
-        final String name = request.getBody().orElse(null);
+  /**
+   * This function listens at endpoint "/api/TelegramWebhook". To invoke it using "curl" command in bash:
+   * curl -d "HTTP Body" {your host}/api/TelegramWebhook
+   */
+  @FunctionName("TelegramWebhook")
+  public HttpResponseMessage run(
+    @HttpTrigger(
+      name = "req",
+      methods = { HttpMethod.POST },
+      authLevel = AuthorizationLevel.FUNCTION
+    ) HttpRequestMessage<Optional<String>> request,
+    final ExecutionContext context
+  ) {
+    context.getLogger().info("Java HTTP trigger processed a request.");
 
-        if (name == null) {
-            return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("Please pass a name in the request body").build();
-        } else {
-            return request.createResponseBuilder(HttpStatus.OK).body("Hello, " + name).build();
-        }
+    final String name = request.getBody().orElse(null);
+
+    if (name == null) {
+      return request
+        .createResponseBuilder(HttpStatus.BAD_REQUEST)
+        .body("Please pass a name in the request body")
+        .build();
+    } else {
+      return request
+        .createResponseBuilder(HttpStatus.OK)
+        .body("Hello, " + name)
+        .build();
     }
+  }
 }
